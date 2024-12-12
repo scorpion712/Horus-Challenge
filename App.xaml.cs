2dd20f8637aeaeadf2e72148450022ea9dae362d
lautaro.diez@sbi-technology.com <lautaro.diez@sbi-technology.com>
2024-12-12 18:41:23 -0300

    initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Horus_Challenge
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Horus_Challenge
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
