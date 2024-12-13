261b6c2b84567eee9f22b3ce0267b2f1c814cecd
lautaro.diez@sbi-technology.com <lautaro.diez@sbi-technology.com>
2024-12-13 11:46:49 -0300

    Routes and DI

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace Horus_Challenge
+﻿using Horus_Challenge.Views;
+
+namespace Horus_Challenge
 {
     public partial class AppShell : Shell
     {
--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Horus_Challenge.Services;
+using Horus_Challenge.Services.Interfaces;
+using Horus_Challenge.ViewModels;
+using Horus_Challenge.Views;
+using Microsoft.Extensions.Logging;
+using Mopups.Hosting;
+using Mopups.PreBaked.Interfaces;
 
 namespace Horus_Challenge
 {
@@ -18,10 +24,27 @@ namespace Horus_Challenge
                     fonts.AddFont("Gilroy-Regular.ttf", "GilroyRegular");
                     fonts.AddFont("Gilroy-SemiBold.ttf", "GilroySemibold");
                 });
+            
+            builder.Services.AddTransient<LoginPage>();
+            builder.Services.AddTransient<LoginViewModel>();
 
+            builder.Services.AddSingleton<HttpClientService>();
+            builder.Services.AddSingleton<IAuthService, AuthService>();
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
+
+
+            // Entries handler to hide underline on Entry
+            Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping(nameof(Entry), (handler, view) =>
+            {
+#if __ANDROID__
+    handler.PlatformView.SetBackgroundColor(Android.Graphics.Color.Transparent);
+#elif __IOS__
+                handler.PlatformView.BackgroundColor = UIKit.UIColor.Clear;
+                handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+#endif
+            });
 
             return builder.Build();
         }
