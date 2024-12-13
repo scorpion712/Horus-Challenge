e37d45ba63734c7faafbb25c7c30311b3adf907f
lautaro.diez@sbi-technology.com <lautaro.diez@sbi-technology.com>
2024-12-13 11:45:14 -0300

    Authentication login service

---
+++ Services/AuthService.cs
@@ -0,0 +1,41 @@
+﻿using Horus_Challenge.Models;
+using Horus_Challenge.Services.Interfaces;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Text;
+using System.Text.Json;
+
+namespace Horus_Challenge.Services;
+
+public class AuthService(HttpClientService httpClientService) : IAuthService
+{
+    private readonly HttpClient client = httpClientService.GetClient();
+    JsonSerializerOptions _serializerOptions;
+
+    public async Task<User?> SignIn(string email, string password)
+    {
+        try
+        {
+            var json = JsonConvert.SerializeObject(new { Email = email, Password = password });
+
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync($"UserSignIn", content);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                Debug.WriteLine("Usuario o contraseña inválidos");
+                return null;
+            }
+
+            var loginResponse = await response.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<User>(loginResponse);
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Ha ocurrido un error al iniciar sesión: {ex.Message}");
+            return null;
+        }
+    }
+}
---
+++ Services/HttpClientService.cs
@@ -0,0 +1,19 @@
+﻿using Horus_Challenge.Helpers.Utils;
+
+namespace Horus_Challenge.Services;
+
+public class HttpClientService
+{
+    private readonly HttpClient _client;
+
+    public HttpClientService()
+    {
+        var httpClientHandler = new HttpClientHandler();
+        _client = new HttpClient(httpClientHandler)
+        {
+            BaseAddress = new Uri(Constants.API_BASE_URL),
+        };
+    }
+
+    public HttpClient GetClient() => _client;
+}
