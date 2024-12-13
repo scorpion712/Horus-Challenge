d72d13aa77991eef244274aa3d9a13613ecf9925
lautaro.diez@sbi-technology.com <lautaro.diez@sbi-technology.com>
2024-12-13 19:19:43 -0300

    Add IsRefreshing prop and go back method

--- ViewModels/ViewModelBase.cs
+++ ViewModels/ViewModelBase.cs
@@ -1,13 +1,18 @@
-﻿namespace Horus_Challenge.ViewModels;
+﻿using Horus_Challenge.Views;
+
+namespace Horus_Challenge.ViewModels;
 
 public partial class ViewModelBase : ObservableObject
 {
     [ObservableProperty]
-    string title;
-
-    [ObservableProperty]
     [NotifyPropertyChangedFor(nameof(IsNotBusy))]
     bool isBusy;
 
+    [ObservableProperty]
+    private bool isRefreshing;
+
     public bool IsNotBusy => !IsBusy;
+
+    [RelayCommand]
+    private async Task GoBack() => await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
 }
