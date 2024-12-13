66dc7bb4795cfb2ba2bf87e2d06133d6c9452639
lautaro.diez@sbi-technology.com <lautaro.diez@sbi-technology.com>
2024-12-13 19:17:09 -0300

    Create Challenge model

---
+++ Models/Challenge.cs
@@ -0,0 +1,13 @@
+﻿namespace Horus_Challenge.Models;
+
+public class Challenge
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public int CurrentPoints { get; set; }
+    public int TotalPoints { get; set; }
+    public double CompletedPercentage => (CurrentPoints * 100) / TotalPoints;
+    public double Progress => CompletedPercentage / 100;
+    public string Status => $"{CurrentPoints}/{TotalPoints}";
+}
