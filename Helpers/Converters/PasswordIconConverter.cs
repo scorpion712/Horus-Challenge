d6585b11d5aa81132fb041fd9fb0c5763355c49c
lautaro.diez@sbi-technology.com <lautaro.diez@sbi-technology.com>
2024-12-13 11:17:23 -0300

    Show or hide password icon converter

---
+++ Helpers/Converters/PasswordIconConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace Horus_Challenge.Helpers.Converters;
+
+public class PasswordIconConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        var showPassword = (bool)value;
+
+        return showPassword ? "eye_b" : "eye_none_b";
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
