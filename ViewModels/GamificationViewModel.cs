c1d951cc4b924d97f1cfa1741655dd6b2803f364
lautaro.diez@sbi-technology.com <lautaro.diez@sbi-technology.com>
2024-12-13 19:20:54 -0300

    Gamification View Model and Page

---
+++ ViewModels/GamificationViewModel.cs
@@ -0,0 +1,65 @@
+﻿using Horus_Challenge.Models;
+using Horus_Challenge.Services.Interfaces;
+using Horus_Challenge.Views.PopUps;
+using Mopups.Services;
+using System.Collections.ObjectModel;
+
+namespace Horus_Challenge.ViewModels;
+
+public partial class GamificationViewModel(IChallengesService challengesService) : ViewModelBase
+{
+    private readonly IChallengesService challengesService = challengesService;
+
+    #region Properties
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(TotalChallenges))]
+    [NotifyPropertyChangedFor(nameof(CompletedChallenges))]
+    ObservableCollection<Challenge> challenges = [];
+    public int TotalChallenges => Challenges.Count;
+    public int CompletedChallenges => Challenges.Count(c => c.CurrentPoints == c.TotalPoints);
+
+    #endregion
+
+    #region Commands
+    [RelayCommand]
+    private async Task OnChallengeTapped(Challenge challenge)
+   => await MopupService.Instance.PushAsync(new AlertPage($"{challenge.Title}: {challenge.Description}", "Reto elegido"));
+
+    [RelayCommand]
+    async Task Refresh()
+    {
+        if (IsRefreshing) return;
+        try
+        {
+            IsRefreshing = true;
+            Challenges = [];
+            await FetchChallenges();
+        }
+        finally
+        {
+            IsRefreshing = false;
+        }
+    }
+    #endregion
+
+    #region Methods
+    public async Task FetchChallenges()
+    {
+        if (IsBusy) return;
+
+        try
+        {
+            IsBusy = true;
+            var challengesResponse = await challengesService.GetAll();
+
+            Challenges = new ObservableCollection<Challenge>(challengesResponse ?? []);
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+
+    } 
+    #endregion
+}
+
---
+++ Views/GamificationPage.xaml.cs
@@ -0,0 +1,32 @@
+using Horus_Challenge.Models;
+using Horus_Challenge.ViewModels;
+
+namespace Horus_Challenge.Views;
+
+public partial class GamificationPage : ContentPage
+{
+	public GamificationPage(GamificationViewModel viewModel)
+	{
+		InitializeComponent();
+		BindingContext = viewModel;
+    }
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        (BindingContext as GamificationViewModel).RefreshCommand.Execute(null);
+    }
+
+    private void OnTappedChallenge(object sender, TappedEventArgs e)
+    {
+        if (sender is Grid tappedElement)
+            if (tappedElement.BindingContext is Challenge tappedItem)
+                ((GamificationViewModel)BindingContext).ChallengeTappedCommand.Execute(tappedItem);
+    }
+
+    private void OnClickedArrowButton(object sender, EventArgs e)
+    {
+        if (sender is ImageButton tappedElement)
+            if (tappedElement.BindingContext is Challenge tappedItem)
+                ((GamificationViewModel)BindingContext).ChallengeTappedCommand.Execute(tappedItem);
+    }
+}
