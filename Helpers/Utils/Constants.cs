93a39d5189cc3e7d5188bb02d96b5a6c95b03b13
lautaro.diez@sbi-technology.com <lautaro.diez@sbi-technology.com>
2024-12-13 11:44:14 -0300

    Email validator helper

---
+++ Helpers/Utils/Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace Horus_Challenge.Helpers.Utils;
+
+public class Constants
+{
+    public static class FormValidator
+    {
+        public const string RequieredField = "Campo requerido";
+        public const string InvalidFormat = "Formato inválido";
+    }
+
+    public static string API_BASE_URL = "https://horuschallenges.azurewebsites.net/api/";
+}
